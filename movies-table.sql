6e185d4c0f2eda123667605d1c49432294eaa8df
Kemal Ekim <59113696+kemoek@users.noreply.github.com>
2022-02-18 18:34:50 +0300

    database table

---
+++ movies-table.sql
@@ -0,0 +1,10 @@
+CREATE TABLE movies (
+  	ranking smallint,
+	title varchar(75),
+  	imdbYear char(4),
+	runTime smallint,
+	genre varchar(50),
+	rating decimal,
+	director varchar(50),
+  PRIMARY KEY (ranking)
+)
